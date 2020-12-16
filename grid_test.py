86b7a5aa6bcc197b1ef3f275ee5b2f9dfd330566
NickFaulkner101 <55847333+NickFaulkner101@users.noreply.github.com>
2020-12-16 13:02:08 +0000

    Add files via upload

--- grid_test.py
+++ grid_test.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-
+import sys
 
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
@@ -14,13 +14,13 @@ from matplotlib.collections import PolyCollection
 r_0 = 63.2
 
 
-
+# https://www.w3resource.com/python-exercises/basic/python-basic-1-exercise-40.php
 
 # ((wakes[j])*np.sin(-U_direction_radians)+(r_0+alpha*wakes[j])*np.cos(-U_direction_radians))+y[i]
 
 "0 degrees is coming from due North."
 "+90 degrees means the wind is coming from due East, -90 from due West"
-U_direction = 180
+U_direction = float(sys.argv[1])
 wind_direction = (U_direction+90) * np.pi / 180.
 
 
@@ -59,7 +59,7 @@ x = 1000
 line1_point1 = turbine_origin
 line1_point2 = equation_of_line1(x,turbine_origin, r_0, wind_direction)
 
-line1_x = [line1_point1[0], line1_point2[0]]
+line1_x = [line1_point1[0]+r_0, line1_point2[0]]
 line1_y = [line1_point1[1], line1_point2[1]]
 
 
@@ -67,7 +67,7 @@ line1_y = [line1_point1[1], line1_point2[1]]
 line2_point1 = turbine_origin
 line2_point2 = equation_of_line2(x,turbine_origin, r_0, wind_direction)
 print(line2_point2)
-line2_x = [line2_point1[0], line2_point2[0]]
+line2_x = [line2_point1[0]-r_0, line2_point2[0]]
 line2_y = [line2_point1[1], line2_point2[1]]
 
 
@@ -75,6 +75,7 @@ line2_y = [line2_point1[1], line2_point2[1]]
 
 plt.plot(line1_x, line1_y)
 plt.plot(line2_x, line2_y)
+plt.axis('scaled')
 plt.show()
 
 
