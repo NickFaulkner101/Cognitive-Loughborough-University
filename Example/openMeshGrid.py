7c288f755f2c7b1e2c7d3dc764986da1b4c0ccb8
NickFaulkner101 <55847333+NickFaulkner101@users.noreply.github.com>
2023-02-14 18:29:54 +0000

    Add files via upload

---
+++ Example/openMeshGrid.py
@@ -0,0 +1,29 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+import os
+
+mycmap = cm.get_cmap('jet')
+mycmap.set_under('w')   
+
+
+X = np.loadtxt(open("X.csv", "rb"), delimiter=",")
+Y = np.loadtxt(open("Y.csv", "rb"), delimiter=",")
+Z = np.loadtxt(open("Z.csv", "rb"), delimiter=",")
+print('Successfuly Loaded!')
+print('Plotting...')
+
+plt.figure(1, figsize=(13,5))
+mycmap = cm.get_cmap('jet')
+mycmap.set_under('w')   
+plt.title('Wind Speed Flow Field')
+plt.pcolormesh(X, Y, Z, shading='auto',cmap=mycmap)
+plt.axis('scaled')
+plt.xlabel('Local x-Direction /m')
+plt.ylabel('Local y-Direction /m')
+cbar = plt.colorbar()
+cbar.set_label('Wind Speed /(m/s)')
+
+plt.show()
